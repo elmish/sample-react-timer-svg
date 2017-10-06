a98c7ae828d2da86a2a6eca3d7d72e4c8379fcf7
Zaid-Ajaj <zaid.naom@gmail.com>
2017-10-06 14:21:35 +0200

    Update sample

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ## SVG Clock [Demo](https://fable-elmish.github.io/sample-react-timer-svg/)
 ![clock.gif](https://cloud.githubusercontent.com/assets/13316248/24984147/011f8420-1fec-11e7-98c7-4005046f174c.gif)
 
-## Build and running the app
+## Building and running the app
 
 1. Install npm dependencies: `yarn install` or `npm install`
-2. Install dotnet dependencies: `dotnet restore`
-3. Start Fable server and Webpack dev server: `dotnet fable npm-run start`
-4. In your browser, open: http://localhost:8080/
+2. Install dotnet dependencies: `.paket/paket.exe install`
+3. Restore references for project `cd src && dotnet restore`
+4. In one shell, Start Fable daemon: `dotnet fable start`
+5. In another shell, Start Webpack dev server: `npm start`
+6. In your browser, open: http://localhost:8080/
 
 Any modification you do to the F# code will be reflected in the web page after saving.
--- src/App.fs
+++ src/App.fs
@@ -9,7 +9,6 @@ open Fable.Core.JsInterop
 type Model = CurrentTime of DateTime
 type Messages = Tick of DateTime
 
-
 open Elmish
 open Elmish.React
 
@@ -31,7 +30,6 @@ type Time =
     | Minute of int
     | Second of int
 
-
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
@@ -44,21 +42,20 @@ let clockHand time color width length =
     let angle = 2.0 * Math.PI * clockPercentage
     let handX = (50.0 + length * cos (angle - Math.PI / 2.0))
     let handY = (50.0 + length * sin (angle - Math.PI / 2.0))
-    line [ X1 (!^ "50"); Y1 (!^ "50"); X2 (!^ (string handX)); Y2 (!^ (string handY)); Stroke color; StrokeWidth (!^ width) ] []
-
+    line [ X1 (!! "50"); Y1 (!! "50"); X2 (!! (string handX)); Y2 (!! (string handY)); Stroke color; !! ("stroke-width", string width) ] []
 
 let handTop n color length fullRound = 
     let revolution = float n
     let angle = 2.0 * Math.PI * (revolution / fullRound)
     let handX = (50.0 + length * cos (angle - Math.PI / 2.0))
     let handY = (50.0 + length * sin (angle - Math.PI / 2.0))
-    circle [ Cx (!^ (string handX)); Cy (!^ (string handY)); R (!^ "2"); Fill color ] []
+    circle [ Cx (!! (string handX)); Cy (!! (string handY)); R (!! "2"); !! ("fill", color) ] []
 
 let view (CurrentTime time) dispatch =
     svg 
       [ ViewBox "0 0 100 100"; unbox ("width", "350px") ]
       [ circle 
-          [ Cx (!^ "50"); Cy (!^ "50"); R (!^ "45"); Fill "#0B79CE" ] 
+          [ Cx (!! "50"); Cy (!! "50"); R (!! "45"); !! ("fill", "#0B79CE") ] 
           []
         // Hours
         clockHand (Hour time.Hour) "lightgreen" "2" 25.0
@@ -71,12 +68,9 @@ let view (CurrentTime time) dispatch =
         handTop time.Second "#023963" 40.0 60.0
         // circle in the center
         circle 
-          [ Cx (!^ "50"); Cy (!^ "50"); R (!^ "3"); Fill "#0B79CE"; Stroke "#023963"; StrokeWidth (!^ "1")  ] 
+          [ Cx (!! "50"); Cy (!! "50"); R (!! "3"); !! ("fill", "#0B79CE") ; Stroke "#023963"; !! ("stroke-width", 1.0) ] 
           []
       ]
-
-
-
 
 // App
 Program.mkProgram initialState update view
